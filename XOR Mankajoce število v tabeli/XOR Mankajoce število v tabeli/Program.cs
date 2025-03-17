14ae8798f3d163f142cffb627297673fe3f91edc
robik2019 <60625071+robik2019@users.noreply.github.com>
2025-03-17 19:23:04 +0100

    Add files via upload

---
+++ XOR Mankajoce število v tabeli/XOR Mankajoce število v tabeli/Program.cs
@@ -0,0 +1,26 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] podaniNiz = { 3, 7, 1, 2, 8, 4, 5 };
+
+        int n = podaniNiz.Length + 1;
+        int xorVse = 0;
+        int xorNiz = 0;
+
+        for (int i = 1; i <= n; i++) 
+        {
+            xorVse ^= i;
+        }
+
+        foreach (int k in podaniNiz)
+        {
+            xorNiz = k;
+        }
+
+        int mankajoceStevilo = xorVse ^ xorNiz;
+
+        Console.WriteLine("Mankajoče število je: " + xorNiz);
+    }
+}
