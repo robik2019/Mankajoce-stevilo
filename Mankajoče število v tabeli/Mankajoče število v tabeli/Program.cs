792b34a54356d672a031714f8b8341d575a0a0ca
robik2019 <60625071+robik2019@users.noreply.github.com>
2025-03-17 15:52:50 +0100

    Add files via upload

---
+++ Mankajoče število v tabeli/Mankajoče število v tabeli/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] podaniNiz = { 3, 7, 1, 2, 8, 4, 5 };
+
+        int n = podaniNiz.Length + 1;
+        int pričakovanaVsota = n * (n + 1) / 2;
+        int dejanskaVsota = podaniNiz.Sum();
+        int mankajoceStevilo = pričakovanaVsota - dejanskaVsota;
+
+        Console.WriteLine("Mankajoče število je: "+mankajoceStevilo);
+    }
+}
